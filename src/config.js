a2c74c57e072dcf245ccc6e4b9490e170af12777
Sean <sean.cowan1@gmail.com>
2020-06-11 15:18:27 -0400

    fixed a typo in the fsocket-client URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     SERVER_URL: process.env.VERCEL_URL || "http://localhost:8000",
-    ORIGIN_URL: "https://fsocket-client.now.sh/" || "http://localhost:3000"
+    ORIGIN_URL: "https://fsocket-client.now.sh" || "http://localhost:3000"
   }
