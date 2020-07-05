e96886aa9cd188bd48ce8813e4d568745d95727c
Sean <sean.cowan1@gmail.com>
2020-07-05 18:25:16 -0400

    updated config urls

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    SERVER_URL: process.env.VERCEL_URL || "http://localhost:8000",
+    SERVER_URL: process.env.HEROKU_URL || "http://localhost:8000",
     ORIGIN_URL: "https://fsocket-client-git-minimum-viable.seancowan-dev.now.sh",
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://dunder_mifflin:password@localhost/fsocket",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://dunder_mifflin:password@localhost/fsocket"
