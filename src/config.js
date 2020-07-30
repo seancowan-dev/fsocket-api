a29f5eab1cf20937eb93129dc36a5114a8a1027e
Sean <sean.cowan1@gmail.com>
2020-07-30 14:43:47 -0400

    changed origin url to prepare for finaly deploy

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     SERVER_URL: process.env.HEROKU_URL || "http://localhost:8000",
-    ORIGIN_URL: "http://localhost:3000",
+    ORIGIN_URL: "https://fsocket-client-git-project-complete.seancowan-dev.now.sh/",
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://dunder_mifflin:password@localhost/fsocket"
   }
 
-  //    ORIGIN_URL: "https://fsocket-client-git-socket-complete.seancowan-dev.now.sh/" - for live
+  //    ORIGIN_URL: "https://fsocket-client-git-project-complete.seancowan-dev.now.sh/" - for live
   //    ORIGIN_URL: "http://localhost:3000" - for dev
